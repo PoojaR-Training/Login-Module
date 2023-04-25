6cde0660cde8673c261276b85ba34b8296d9a16f
iMac <imac@iMacs-iMac.local>
2023-04-25 11:48:19 +0530

    commit

--- App.js
+++ App.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import {View, Text, StyleSheet,Button} from 'react-native';
-import exstyles from './exstyles'
-
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, ScrollView, SafeAreaView} from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen/SignUpScreen';
+import EmailConfirm from './src/screens/EmailConfirmScreen/EmailConfirm';
+import ForgetPasswordScreen from './src/screens/ForgetPasswordScreen/ForgetPasswordScreen';
+import NewPasswordScreen from './src/screens/NewPasswordScren/NewPasswordScreen';
+import Navigation from './src/navigation';
 const App = () => {
  
   return (
-    <View>
-      <Text style={{fontSize : 30, marginTop : 50, marginLeft:10, textAlign :"center", backgroundColor:"orange"}}>StyleSheet Example</Text>
-      <Text style={styles.textBox}>StyleSheet</Text>
-      <Text style={exstyles.textBox}>StyleSheet</Text>
-    </View>
-    
+   
+      <SafeAreaView style={styles.main}>
+    <Navigation/>
+    </SafeAreaView>
   );
-  
 };
 
 const styles = StyleSheet.create({
-  textBox :{
-    fontSize: 30,
-    marginTop : 50, 
-    marginLeft:10,
-    backgroundColor:"lightblue" ,
-    color:"blue" ,
-    borderRadius : 20,
-    overflow: 'hidden',
-    borderColor  :"red",
-    textAlign : "center"
-  }
+  main: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: '#dee1e3',
+  },
 });
-
 
 export default App;
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+
+export default HomeScreen;
